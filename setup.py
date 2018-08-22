04f242cf5a686847fa756585c9bb22cf0e7765d9
vitalije <vitalije@kviziracija.net>
2018-08-22 18:40:33 +0200

    updated documentation

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#@+leo-ver=5-thin
+#@+node:vitalije.20180822183813.1: * @file setup.py
+#@@first
+#@@first
 
+#@+others
+#@+node:vitalije.20180822183827.1: ** Declarations (setup.py)
 """The setup script."""
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = ['Click>=6.0', ]
@@ -25,11 +31,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
@@ -53,3 +55,7 @@ setup(
     version='0.1.0',
     zip_safe=False,
 )
+#@-others
+#@@language python
+#@@tabwidth -4
+#@-leo
