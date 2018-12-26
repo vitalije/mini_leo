d648b7c8b7340a3f919103060a9a27872518cbd2
vitalije <vitalije@kviziracija.net>
2018-12-26 09:36:18 +0100

    update rust-installer.sh

--- rust-installer.sh
+++ rust-installer.sh
@@ -2,4 +2,4 @@
 mkdir -p ~/rust-installer
 curl -sL https://static.rust-lang.org/rustup.sh -o ~/rust-installer/rustup.sh
 export RUSTUP_PREFIX=~/rust
-sh ~/rust-installer/rustup.sh install stable --disable-sudo -y
+sh ~/rust-installer/rustup.sh --default-toolchain stable -y
