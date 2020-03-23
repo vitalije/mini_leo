353ee99f6863e6f7539315526463f570cb44fcd2
vitalije <vitalije@kviziracija.net>
2020-03-23 10:28:20 +0100

    trying to fix .travis.yml

--- rust/src/atclean.rs
+++ rust/src/atclean.rs
@@ -1,9 +1,17 @@
 use crate::model::{VData, Outline}; //, OutlineOps, LevGnx, LevGnxOps};
+pub fn atclean_to_string(outline:&Outline, nodes:&Vec<VData>, ni:usize) -> String {
+  if nodes.len() > 0 && ni < outline.len() {
+    String::new()
+  } else {
+    String::new()
+  }
+}
 pub fn update_atclean_tree(outline:&Outline, nodes:&mut Vec<VData>, cont:&str) {
-  let _v = nodes.get_mut(0);
-  if cont.len() == 0 || outline.len() == 0 {
-    for v in _v {
-      v.b.clear();
-    }
+  let oldtxt = atclean_to_string(outline, nodes, 0);
+  let v = nodes.get_mut(0).unwrap();
+  if cont.len() == 0 {
+    v.b.push_str(cont);
+  } else {
+    v.b.push_str(oldtxt.as_str());
   }
 }
